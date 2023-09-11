d8c36b3b4a5f5a2ceb12af0605046565fcb69c8c
jsejaldin@gmail.com <joel.jaldin@avenida.com>
2023-09-11 16:31:12 -0300

    fetch and show users from API

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sunt
+        repellendus fuga necessitatibus similique fugiat? Repudiandae officiis
+        labore, ut quidem consequatur ab quisquam, cumque obcaecati commodi
+        aliquid distinctio perspiciatis possimus, maxime eos eaque. At soluta
+        iste blanditiis sunt, expedita ducimus unde esse quod natus, sint
+        mollitia officiis laboriosam, dolor ipsum eligendi vero laborum
+        excepturi recusandae debitis? Sunt itaque perspiciatis sapiente
+        earum magni, modi explicabo perferendis neque veniam at soluta
+        similique pariatur quas natus ipsum sint voluptatem esse asperiores
+        facilis architecto in minima quod corporis! Animi fuga, dicta veritatis,
+         tempore voluptate laborum architecto quibusdam, quidem distinctio
+         officia excepturi temporibus accusamus quae.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti at
+        quae eius rem, modi odit aliquid, provident quo explicabo quis illo.
+        Architecto vitae molestiae doloremque ab voluptatem sit iusto excepturi
+        voluptate, dolore provident debitis voluptatibus hic exercitationem
+        eveniet! Corrupti quaerat eum adipisci modi reiciendis consectetur
+        veritatis ipsa provident non sint.</p>
+    </div>
+  )
+}
+
+export default ServicePage
